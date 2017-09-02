e2838f75faf251c9ea235652e349fedce41410d9
Marshall Houston <marshall.houston@gmail.com>
2017-09-02 15:49:42 -0500

    adds initial condition based on user_input

---
+++ lib/battleship.rb
@@ -0,0 +1,26 @@
+class Battleship
+
+  def initialize
+    puts "Welcome to BATTLESHIP\n
+    Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    user_input = gets.chomp
+    if user_input == "p" || user_input == "play"
+      p "Great, let's play."
+      #play the game.... instantiate a new play instance
+    elsif user_input == "i" || user_input == "instruction" || user_input == "instructions"
+      p "Sweet, here are the instructions."
+      #start a new instance of the instructions class.
+    elsif user_input == "q" || user_input == "quit"
+      p "Ok quitter, bye!"
+      # exit the game
+    else
+      "I didn't understand what you said. Try again."
+      user_input = gets.chomp
+    end
+
+  end
+
+
+end
+
+Battleship.new
